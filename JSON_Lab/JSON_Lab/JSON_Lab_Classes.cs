2ead672966f4e7fb7767ecaa55c246e1dfe59135
fg1027507 <fg1027507@otc.edu>
2026-02-10 19:19:24 -0600

    Add files via upload

---
+++ JSON_Lab/JSON_Lab/JSON_Lab_Classes.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Person
+{
+    public string name { get; set; }
+    public int Age { get; set; }
+    public string email { get; set; }
+    public bool isStudent { get; set; }
+}
---
+++ JSON_Lab/JSON_Lab/Program.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person
+        {
+            name = "Finn Gilbert",
+            Age = 16,
+            email = "fg1027507@otc.edu",
+            isStudent = true
+        };
+
+        string json = JsonConvert.SerializeObject(person);
+        Console.WriteLine("Serialized JSON: " + json);
+    }
+}
