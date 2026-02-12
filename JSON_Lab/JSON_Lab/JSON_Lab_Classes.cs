5863c7523c966c799a55df0623d58e4858e23063
fg1027507 <fg1027507@otc.edu>
2026-02-12 09:22:10 -0600

    Add newline at end of JSON_Lab_Classes.cs

--- JSON_Lab/JSON_Lab/JSON_Lab_Classes.cs
+++ JSON_Lab/JSON_Lab/JSON_Lab_Classes.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 
 public class Person
 {
+    // Properties of the Person class
     public string name { get; set; }
     public int Age { get; set; }
     public string email { get; set; }
